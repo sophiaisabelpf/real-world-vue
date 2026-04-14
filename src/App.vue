7aea034da9174501ef5a298de86b8f73c083bfe7
Sophia Ferrer <sophiaf@Sophias-MBP.localdomain>
2026-04-14 16:49:25 -0400

    Create project and add db.json

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Events</RouterLink> |
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+  </div>
+</template>
+
+<style scoped>
+#layout {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+h2 {
+  font-size: 20px;
+}
+</style>
